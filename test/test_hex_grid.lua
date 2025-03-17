b35e6d39152dfe7769f5f6de6a2cb4ce6cd1e6be
EASYept <bybl1k@yandex.ru>
2025-03-18 01:01:15 +0400

    update tests

--- test/test_hex_grid.lua
+++ test/test_hex_grid.lua
@@ -2,8 +2,8 @@ local test_subject = require "hex_grid.hex_grid_utils"
 
 return function()
 
-	describe("asd", function()
-		test("asd", function()
+	describe("hex grid helper functions test cases", function()
+		test("assert coordinate by pixel positions", function()
 			local size = 50
 			local col, row = test_subject.pixel_to_even_r_hex_coordinates(0, 0, size)
 			assert(col == 1)
@@ -16,16 +16,16 @@ return function()
 			col, row = test_subject.pixel_to_even_r_hex_coordinates(- size, 0, size)
 			assert(col == 0)
 			assert(row == 1)
-
-			
 		end)
 		
 		test("test direction differences odd", function()
-			local empty_around = {	[5] = {[5] = "tile" }}
+			local target_row = 5
+			local target_column = 5
+			local empty_around = {	[target_row] = {[target_column] = "tile" }}
 			local coords = {
-				[5] = {
+				[target_row] = {
 					[4] = "tile", 
-					[5] = "tile", 
+					[target_column] = "tile", 
 					[6] = "tile",
 				}, 
 				[4] = {
@@ -38,11 +38,38 @@ return function()
 				} 
 			}
 			
-			local tiles = test_subject.neighbor_tiles(empty_around, 5, 5)
+			local tiles = test_subject.neighbor_tiles(empty_around, target_row, target_column)
 			assert(#tiles == 0, "expecting 0 tiles around 5,5 coords is returned")
 
-			tiles = test_subject.neighbor_tiles(coords, 5, 5)
+			tiles = test_subject.neighbor_tiles(coords, target_row, target_column)
 			assert(#tiles == 6, "expecting all tiles around 5,5 coords is returned")
+		end)
+		
+		test("test direction differences even", function()
+			local target_row = 6
+			local target_column = 6
+			local empty_around = {	[target_row] = {[target_column] = "tile" }}
+			local coords = {
+				[5] = {
+					[5] = "tile", 
+					[6] = "tile",
+				},
+				[target_row] = {
+					[5] = "tile", 
+					[target_column] = "tile",
+					[7] = "tile",
+				},
+				[7] = {
+					[5] = "tile", 
+					[6] = "tile",
+				} 
+			}
+
+			local tiles = test_subject.neighbor_tiles(empty_around, target_row, target_column)
+			assert(#tiles == 0, "expecting 0 tiles around 6,6 coords is returned")
+
+			tiles = test_subject.neighbor_tiles(coords, target_row, target_column)
+			assert(#tiles == 6, "expecting all tiles around 6,6 coords is returned")
 		end)
 	end)
 end
